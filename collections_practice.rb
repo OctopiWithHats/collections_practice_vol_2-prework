49cb85a8090f35621d70e2ac1825c9896e937438
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 23:48:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,24 +1,24 @@
 # your code goes here
-def begins_with_r(arr)
-    arr.all? do |string| 
+def begins_with_r(array)
+    array.all? do |string| 
     string[0] == "r"
   end
 end
 
- def contain_a(arr)
-    arr.select do |string|
+ def contain_a(array)
+    array.select do |string|
     string.include?("a")
   end
 end
 
- def first_wa(arr)
-    arr.detect do |string|
-    string[0,2] == "wa"
+ def first_wa(array)
+    array.detect do |string|
+    string[0, 2] == "wa"
   end
 end
 
- def remove_non_strings(arr)
-    arr.delete_if do |string| 
+ def remove_non_strings(array)
+    array.delete_if do |string| 
     string.is_a?(String) == false
   end
 end
